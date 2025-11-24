9eb742b806ee0fc67db1f9b654689b398d6da21c
minarai847 <guretodibaidinngu@yahoo.co.jp>
2025-11-24 11:44:30 +0900

    Fix user controller and reorganize usecase package
    - Fix method name: LogIn -> Login to match interface
    - Move user_usecase.go to usecase/ directory
    - Add Echo framework dependency
    - Fix import paths

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -1,9 +1,11 @@
 package controller
 
 import (
-	usecase "go-rest-api"
 	"go-rest-api/model"
+	"go-rest-api/usecase"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -14,12 +16,12 @@ type IUserController interface {
 	LogOut(c echo.Context) error
 }
 
-type userContoroller struct {
+type userController struct {
 	uu usecase.IUserUsecase
 }
 
 func NewUserController(uu usecase.IUserUsecase) IUserController {
-	return &userContoroller{uu}
+	return &userController{uu}
 }
 
 func (uc *userController) SignUp(c echo.Context) error {
@@ -27,4 +29,45 @@ func (uc *userController) SignUp(c echo.Context) error {
 	if err := c.Bind(&user); err != nil {
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
+	userRes, err := uc.uu.SignUp(user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusCreated, userRes)
+}
+
+func (uc *userController) Login(c echo.Context) error {
+	user := model.User{}
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	tokenString, err := uc.uu.Login(user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	cookie := new(http.Cookie)
+	cookie.Name = "token"
+	cookie.Value = tokenString
+	cookie.Expires = time.Now().Add(24 * time.Hour)
+	cookie.Path = "/"
+	cookie.Domain = os.Getenv("API_DOMAIN")
+
+	cookie.HttpOnly = true
+	cookie.SameSite = http.SameSiteNoneMode
+	c.SetCookie(cookie)
+	return c.NoContent(http.StatusOK)
+}
+
+func (uc *userController) LogOut(c echo.Context) error {
+	cookie := new(http.Cookie)
+	cookie.Name = "token"
+	cookie.Value = ""
+	cookie.Expires = time.Now()
+	cookie.Path = "/"
+	cookie.Domain = os.Getenv("API_DOMAIN")
+	cookie.HttpOnly = true
+	cookie.SameSite = http.SameSiteNoneMode
+	c.SetCookie(cookie)
+	return c.NoContent(http.StatusOK)
+
 }
