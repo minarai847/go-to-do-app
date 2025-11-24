b75d7ddc6dcf297f5a5e57a42caab278454460ef
minarai847 <guretodibaidinngu@yahoo.co.jp>
2025-11-24 14:30:22 +0900

    Add main.go, router, and task repository
    - Add main.go with application entry point
    - Add router package with route definitions
    - Add task repository interface and implementation
    - Fix signup route typo (/sinup -> /signup)

---
+++ repository/task_repository.go
@@ -0,0 +1,23 @@
+package repository
+
+import (
+	"go-rest-api/model"
+
+	"gorm.io/gorm"
+)
+
+type ITaskRepository interface {
+	GetAllTasks(tasks *[]model.Task, userId uint) error
+	GetTaskById(task *model.Task, userId uint, taskId uint) error
+	CreateTask(task *model.Task) error
+	UpdateTask(task *model.Task, userId uint, taskId uint) error
+	DeleteTask(userId uint, taskId uint) error
+}
+
+type taskRepository struct {
+	db *gorm.DB
+}
+
+func NewTaskRepository(db *gorm.DB) ITaskRepository {
+	return &taskRepository{db}
+}
