74334d60b723cf62ca7889ddee7f0fa286f204f2
minarai847 <guretodibaidinngu@yahoo.co.jp>
2025-12-07 10:38:24 +0900

    Fix Todo component: remove unused ShieldCheckicon import

--- frontend/src/components/Todo.tsx
+++ frontend/src/components/Todo.tsx
@@ -1,7 +1,19 @@
-
+import {
+    ArrowRightOnRectangleIcon,
+} from '@heroicons/react/24/outline'
+import { useMutateAuth } from '../hooks/useMatateAuth'
 
 export const Todo = () => {
+    const { logoutMutation } = useMutateAuth()
+    const logout = async () => {
+        await logoutMutation.mutateAsync()
+    }
     return (
-        <div>Todo</div>
+        <div>
+            <ArrowRightOnRectangleIcon
+                onClick={logout}
+                className="w-6 h-6 my-6 text-blue-500 cursor-pointer"
+            />
+        </div>
     )
 }
